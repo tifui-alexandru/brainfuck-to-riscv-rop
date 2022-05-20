deb59aed92750673d739b251477e13b63f2beffc
tifui-alexandru <itifui00@gmail.com>
2022-05-20 16:32:29 +0300

    offset added

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from bf_parser import *
 
 p = BF_Parser("><")
 
-print(p.parse())
+offset = ""
+offset = bytes(input("Please provide offset until stack smash:\n"), 'utf-8')
+
+print(offset + p.parse())
