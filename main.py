5ab37548208a5e4988d864547fd815e34e45c5d5
tifui-alexandru <itifui00@gmail.com>
2022-05-20 16:37:32 +0300

    offset changed

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from bf_parser import *
 p = BF_Parser("><")
 
 offset = b"A" * 23 # offset until stack smash ; added for debugging
+payload = offset + p.parse()
 
-print(offset + p.parse())
+print(payload)
+
+with open("input.txt", "wb") as fout:
+    fout.write(payload)
