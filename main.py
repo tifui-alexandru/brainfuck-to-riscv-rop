309fbbd20a7f3c870d634c59f75f7242f12d193a
tifui-alexandru <itifui00@gmail.com>
2022-05-20 17:47:04 +0300

    modification

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ p = BF_Parser("><")
 offset = b"A" * 23 # offset until stack smash ; added for debugging
 payload = offset + p.parse()
 
-print(payload)
+num_bytes = len(payload)
+
+print(f"The payload has {num_bytes} bytes and can be found in the input.txt file")
+# print(payload)
 
 with open("input.txt", "wb") as fout:
     fout.write(payload)
