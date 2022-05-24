7b023ed20d11b9c698dcee9621f90c75fa769997
tifui-alexandru <itifui00@gmail.com>
2022-05-24 13:38:23 +0300

    changed sp

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ tape = b"\x00" * 1024 # the brainfuck tape
 backup_space = b"\x00" # for write_mem and read_mem operations
 
 payload_len = p.get_payload_len()
-sp_addr = 0x3ffffff030
+sp_addr = 0x3ffffff020
 pointer_start = sp_addr + len(offset) + payload_len + 512 # the middle of the tape
 backup_addr = sp_addr + len(offset) + payload_len + 1025 # right after the tape
 
