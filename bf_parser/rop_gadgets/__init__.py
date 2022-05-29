7f687d5020408b32ae377b125049bd034886066c
tifui-alexandru <itifui00@gmail.com>
2022-05-29 16:05:37 +0300

    update gadgets again

--- bf_parser/rop_gadgets/__init__.py
+++
@@ -1,6 +0,0 @@
-import bf_parser.rop_gadgets.and_a3_s0
-import bf_parser.rop_gadgets.charger
-import bf_parser.rop_gadgets.copy_a3
-import bf_parser.rop_gadgets.mov_a0_s0
-import bf_parser.rop_gadgets.restore_a3
-import bf_parser.rop_gadgets.store_a0
