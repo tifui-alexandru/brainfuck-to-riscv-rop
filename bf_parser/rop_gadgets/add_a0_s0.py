870f2f9a3b21f19ef650e840c528644f5b42a09f
tifui-alexandru <itifui00@gmail.com>
2022-06-02 11:39:14 +0300

    fixed add a0 s0 problem

--- bf_parser/rop_gadgets/add_a0_s0.py
+++ bf_parser/rop_gadgets/add_a0_s0.py
@@ -2,13 +2,14 @@ from bf_parser.rop_gadgets_base_class import ROP_Gadget
 
 class Add_A0_S0(ROP_Gadget):
     '''
+    ld ra, 0x8(sp)
     add a0, a0, s0
     ld s0, 0(sp)
     addi sp, sp, 0x10
     jr ra
     '''
     def __init__(self):
-        super().__init__(0x20e10, 0x10)
+        super().__init__(0x20e0e, 0x10)
 
     def construct_frame(self, ra=0, s0=0):
         data = [s0, ra]
